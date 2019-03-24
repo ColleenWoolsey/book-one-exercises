d1b6f2cc88c7d64f62c582c98362f4c2b20270e2
Colleen Woolsey <rlwool@aol.com>
2019-03-24 00:05:37 -0500

    Departments & Employees Complete

---
+++ DepartmentsAndEmployees/DepartmentsAndEmployees/Models/Department.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DapperDepartments.Models
+{
+    // C# representation of the Department table
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+    }
+}
